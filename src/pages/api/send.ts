8a79fcb4814effee2f87f3099ce33dac91fa065f
hmirza-git <hassanmirza621@gmail.com>
2025-09-16 01:55:45 +0500

    api files deleted

--- src/pages/api/send.ts
+++
@@ -1,7 +0,0 @@
-// import type { APIRoute } from "astro";
-
-// export const GET: APIRoute = async () => {
-//     // send an email
-    
-//     return new Response(JSON.stringify({success: true}))
-// };
